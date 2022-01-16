d58782d35150fb3b831c963dc89a3a949fbff9c0
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2022-01-16 16:47:37 +0600

    nothing works

--- add-on/js/background.js
+++ add-on/js/background.js
@@ -3,6 +3,13 @@ function handleActivated(tab) {
     browser.tabs.get(tab.tabId, e=>{
         console.log(e.url)
     })
+    browser.tabs.executeScript({
+        code:`var x=document.getElementsByTagName("h1");
+for(let i=0; i<x.length;i++){
+    console.log(5);
+}`,
+    })
 }
+
 
 browser.tabs.onActivated.addListener(handleActivated);
